a7e4aa98325114b8e98abbf127d6cff49103e360
Jay Estillore <estillorejunnil09@gmail.com>
2023-11-04 22:56:06 +0800

    ASP.NET CORE API with Angular 11042023

---
+++ TodoApp.API/TodoApp.API/TodoApp.API/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.API.Models
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string? Description { get; set; }   
+        public DateTime CreatedDate { get; set; }
+        public bool Iscompleted { get; set;}
+        public DateTime? CompletedDate { get; set; }
+    }
+}
